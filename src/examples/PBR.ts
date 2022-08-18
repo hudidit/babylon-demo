a8744ed68b257d22a0cb0f4f97bbc08ae5069265
hudidit <huyuexyz@gmail.com>
2022-08-19 00:55:37 +0800

    feat: more pbr material

--- src/examples/PBR.ts
+++ src/examples/PBR.ts
@@ -1,4 +1,4 @@
-import { CubeTexture, Engine, FreeCamera, HemisphericLight, MeshBuilder, PBRMaterial, Scene, Texture, Vector3 } from "@babylonjs/core";
+import { Color3, CubeTexture, Engine, FreeCamera, GlowLayer, HemisphericLight, MeshBuilder, PBRMaterial, Scene, Texture, Vector3 } from "@babylonjs/core";
 
 export class PBR {
 
@@ -73,8 +73,40 @@ export class PBR {
 
   createMagic(): PBRMaterial {
     const pbr = new PBRMaterial('pbr', this.scene);
-    pbr.roughness = 1;
-    pbr.environmentIntensity = 0.25;
+    pbr.roughness = 0.3;
+    pbr.environmentIntensity = 1;
+    const uvScale = 3;
+    const texArray: Texture[] = [];
+    
+    const albedoTexture = new Texture('./textures/MetalPipeWallRusty/MetalPipeWallRusty_basecolor.png', this.scene); 
+    pbr.albedoTexture = albedoTexture;
+    texArray.push(albedoTexture);
+
+    const bumpTexture = new Texture('./textures/MetalPipeWallRusty/MetalPipeWallRusty_normal.png', this.scene);
+    pbr.bumpTexture = bumpTexture;
+    texArray.push(bumpTexture);
+
+    // pbr.useAmbientOcclusionFromMetallicTextureRed = true;
+    // pbr.useRoughnessFromMetallicTextureGreen = true;
+    // pbr.useMetallnessFromMetallicTextureBlue = true;
+    // pbr.metallicTexture = new Texture('./textures/MetalPipeWallRusty/MetalPipeWallRusty_ambientocclusion.png', this.scene);
+
+    // pbr.emissiveColor = new Color3(1, 1, 1);
+    // 这个 emissive.png 是一张纯黑图片，没有发光效果。素材有问题，需要使用能发光的素材。
+    // pbr.emissiveTexture = new Texture('./textures/MetalPipeWallRusty/MetalPipeWallRusty_emissive.png', this.scene);
+    // pbr.emissiveIntensity = 3;
+
+    texArray.forEach((tex) => {
+      tex.uScale = uvScale;
+      tex.vScale = uvScale;
+    });
+
+    // 没有 emissiveTexture 时，glowLayer 没有效果
+    // const glowLayer = new GlowLayer('glow', this.scene);
+    // glowLayer.intensity = 1;
+    
+    pbr.invertNormalMapX = true;
+    pbr.invertNormalMapY = true;
     return pbr;
   }
 }
