1f71b0044382e3e17acdd9df72c7d140c109ae63
hudidit <huyuexyz@gmail.com>
2023-03-30 23:31:20 +0800

    feat: add materials; more objects

--- src/examples/BasicScene.ts
+++ src/examples/BasicScene.ts
@@ -1,4 +1,20 @@
-import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3, PointLight, DirectionalLight, SpotLight, Color3, ArcRotateCamera, ShadowGenerator, StandardMaterial } from "@babylonjs/core";
+import {
+  Engine,
+  FreeCamera,
+  HemisphericLight,
+  MeshBuilder,
+  Scene,
+  Vector3,
+  PointLight,
+  DirectionalLight,
+  SpotLight,
+  Color3,
+  ArcRotateCamera,
+  ShadowGenerator,
+  StandardMaterial,
+  Texture,
+  Mesh
+} from "@babylonjs/core";
 
 export class BasicScene {
 
@@ -16,6 +32,9 @@ export class BasicScene {
 
   createScene() {
     const scene = new Scene(this.engine);
+    /**
+     * Cameras
+     */
     // const camera = new FreeCamera('camera', new Vector3(0, 3, -10), this.scene);
     // camera.attachControl();
     // camera.speed = 0.25;
@@ -26,7 +45,28 @@ export class BasicScene {
     camera.upperRadiusLimit = 150;
     camera.attachControl();
 
+    /**
+     * Materials & Textures
+     */
+    const pat = Mesh.FLIP_TILE;
+    const av = Mesh.TOP;
+    const ah = Mesh.LEFT;
 
+    const wallMat = new StandardMaterial('bricks', scene);
+    const wallTexture = new Texture('https://assets.babylonjs.com/environments/bricktile.jpg', scene);
+    // wallTexture.uScale = 3;
+    // wallTexture.vScale = 3;
+    wallMat.diffuseTexture = wallTexture;
+    
+    const groundMat = new StandardMaterial('bricks', scene);
+    const groundTexture = new Texture('https://assets.babylonjs.com/environments/bricktile.jpg', scene);
+    groundTexture.uScale = 20;
+    groundTexture.vScale = 20;
+    groundMat.diffuseTexture = groundTexture;
+
+    /**
+     * Objects
+     */
     const ball1 = this.createBall({
       position: new Vector3(0, 1, 0)
     }, scene);
@@ -35,6 +75,9 @@ export class BasicScene {
       position: new Vector3(0, 3, 2)
     }, scene);
 
+    /**
+     * Ground
+     */
     const ground = MeshBuilder.CreateGround(
       'ground',
       {
@@ -44,12 +87,31 @@ export class BasicScene {
       scene
     );
     ground.receiveShadows = true;
+    ground.material = groundMat;
 
-    const wall = MeshBuilder.CreateBox('wall', { size: 1 }, scene);
+    /**
+     * Wall
+     */
+    // 创建墙的方式 - 1
+    // const wall = MeshBuilder.CreateBox('wall', { size: 1 }, scene);
     // 降盒子在 x, y 方向拉伸，形成薄片（墙）的效果
-    wall.scaling.y = 20;
-    wall.scaling.x = 20;
-    wall.scaling.z = 0.1;
+    // wall.scaling.y = 20;
+    // wall.scaling.x = 20;
+    // wall.scaling.z = 0.1;
+    // 创建墙的方式 - 2, 比第一种简洁
+    // const wall = MeshBuilder.CreateBox('wall', { width: 20, height: 20, depth: 0.1 }, scene);
+    const wall = MeshBuilder.CreateTiledBox('wall', { 
+      width: 20, 
+      height: 20, 
+      depth: 0.1,
+      pattern: pat,
+      sideOrientation: Mesh.DOUBLESIDE,
+      alignVertical: av,
+      alignHorizontal: ah,
+      tileSize: 3,
+      tileWidth: 3
+    }, scene);
+    wall.material = wallMat;
     wall.position.y = 5;
     wall.position.x = 0;
     wall.position.z = -5;
@@ -57,21 +119,42 @@ export class BasicScene {
     wall.rotation.x = Math.PI / -3;
     wall.receiveShadows = true;
 
+
+    const options = {
+      sideOrientation: Mesh.DOUBLESIDE,
+      pattern: pat,
+      alignVertical: av,
+      alignHorizontal: ah,
+      width: 6,
+      height: 3,
+      depth: 1,
+      tileSize: 1,
+      tileWidth: 3
+    }
+
+    const brickBox = MeshBuilder.CreateTiledBox("brickBox", options);
+    brickBox.material = wallMat;
+    brickBox.position.x = -5;
+    brickBox.position.y = 5;
+    brickBox.position.z = 3;
+    brickBox.receiveShadows = true;
+
     /**
      * https://doc.babylonjs.com/features/featuresDeepDive/lights/lights_introduction
      */
     // const hemiLight = new HemisphericLight('hemiLight', new Vector3(0, 0, 0), scene);
-    // const hemiLight = new HemisphericLight('hemiLight', new Vector3(1, 1, 1), scene);
-    // hemiLight.intensity = 0.2;
+    const hemiLight = new HemisphericLight('hemiLight', new Vector3(1, 1, 1), scene);
+    hemiLight.intensity = 0.1;
     // const pointLight = new PointLight("pointLight", new Vector3(3, 5, 0), scene);
     // pointLight.intensity = 0.7;
     const diretionalLight = new DirectionalLight('directionalLight', new Vector3(-10, -20, -15).normalize(), scene);
     diretionalLight.position = new Vector3(10, 20, 10);
     diretionalLight.intensity = 0.7;
-    const spotLight = new SpotLight('spotLight', new Vector3(0, 15, 0), new Vector3(0, -1, 0), Math.PI / 2, 50, scene);
+    const spotLight = new SpotLight('spotLight', new Vector3(0, 15, 0), new Vector3(-0.5, -1, 0), Math.PI / 2, 50, scene);
     spotLight.intensity = 0.9;
     spotLight.diffuse = new Color3(1, 0, 0);
     spotLight.specular = new Color3(0, 1, 0);
+
     // // spotLight.excludedMeshes.push(ball1);
     // const spotLight2 = new SpotLight('spotLight', new Vector3(-5, 15, -5), new Vector3(1, -2, 1), Math.PI / 4, 50, scene);
     // spotLight2.intensity = 0.9;
@@ -98,6 +181,7 @@ export class BasicScene {
     shadowGenerator.addShadowCaster(ball1, true);
     shadowGenerator.addShadowCaster(ball2, true);
     shadowGenerator.addShadowCaster(wall, true);
+    shadowGenerator.addShadowCaster(brickBox, true);
     // shadowGenerator.usePoissonSampling = true;
     // shadowGenerator.useExponentialShadowMap = true;
     // 注意，有两个光源同时生成阴影时，渲染会明显变卡顿
@@ -105,13 +189,15 @@ export class BasicScene {
     shadowGenerator2.addShadowCaster(ball1, true);
     shadowGenerator2.addShadowCaster(ball2, true);
     shadowGenerator2.addShadowCaster(wall, true);
+    shadowGenerator2.addShadowCaster(brickBox, true);
 
     // Animations
     var alpha = 0;
     scene.registerBeforeRender(function () {
       ball1.position = new Vector3(Math.cos(alpha) * 5, 10, Math.sin(alpha) * 5);
-      lightToShadow.position = new Vector3(Math.cos(alpha) * 5, 20, Math.sin(alpha) * 5);
-      lightSphere.position = new Vector3(Math.cos(alpha) * 5, 20, Math.sin(alpha) * 5);
+      const lightSpherePosition = new Vector3(Math.cos(alpha) * 5 + 5, 20, Math.sin(alpha) * 5);
+      lightToShadow.position = lightSpherePosition;
+      lightSphere.position = lightSpherePosition;
       alpha += 0.05;
     });
 
